d1036054688efe30701391c6122462c77541d8e3
cornernote <cornernote@gmail.com>
2015-08-08 01:55:27 +0930

    fix

--- app/config.php
+++ app/config.php
@@ -6,7 +6,7 @@ $config = array(
     'database' => array(
         'models' => '../app/models',
         'connection' => array(
-            'production' => 'pgsql://' . $url['user'] . ':' . $url['pass'] . '@' . $url['host'] . '/' . substr($url['path'], 1)',
+            'production' => 'pgsql://' . $url['user'] . ':' . $url['pass'] . '@' . $url['host'] . '/' . substr($url['path'], 1),
             'development' => 'sqlite://../app/data/bower.db.sqlite',
         ),
     ),
