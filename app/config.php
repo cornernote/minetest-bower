57c2d8a8e34ab5650d11bf192f7e256ec33feed9
cornernote <cornernote@gmail.com>
2015-08-08 01:53:03 +0930

    use pgsql

--- app/config.php
+++ app/config.php
@@ -1,17 +1,13 @@
 <?php
 
 // CONFIG
-
+$url = parse_url(getenv('DATABASE_URL'));
 $config = array(
-
     'database' => array(
-
         'models' => '../app/models',
-
         'connection' => array(
-            'development' => 'sqlite://../app/data/bower.db.sqlite'
-        )
-
-    )
-
+            'production' => 'pgsql://' . $url['user'] . ':' . $url['pass'] . '@' . $url['host'] . '/' . substr($url['path'], 1)',
+            'development' => 'sqlite://../app/data/bower.db.sqlite',
+        ),
+    ),
 );
