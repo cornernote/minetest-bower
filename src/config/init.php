309c48d965b23ec62bee9723f11d6908fe3571de
cornernote <cornernote@gmail.com>
2015-08-10 21:50:52 +0930

    .env file is optional

--- src/config/init.php
+++ src/config/init.php
@@ -1,6 +1,8 @@
 <?php
 require(__DIR__ . '/../../vendor/autoload.php');
-(new \Dotenv\Dotenv(__DIR__ . '/../..'))->load();
+
+if (file_exists(__DIR__ . '/../..'))
+    (new \Dotenv\Dotenv(__DIR__ . '/../..'))->load();
 
 defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG') ? (getenv('YII_DEBUG') == 'false' ? false : getenv('YII_DEBUG')) : true);
 defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV') ? getenv('YII_ENV') : 'dev');
