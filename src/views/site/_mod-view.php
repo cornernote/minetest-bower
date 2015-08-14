510832c057abe6206849480ef4f2b013e8d0a043
cornernote <cornernote@gmail.com>
2015-08-15 00:44:08 +0930

    fix links

--- src/views/site/_mod-view.php
+++ src/views/site/_mod-view.php
@@ -12,13 +12,13 @@ use yii\helpers\Html;
     <div class="thumbnail">
         <?php
         if ($model->screenshots) {
-            echo Html::a(Html::img($model->screenshots[0]), ['view', 'name' => $model->name]);
+            echo Html::a(Html::img($model->screenshots[0]), ['/mod/view', 'name' => $model->name]);
         } else {
-            echo Html::a(Html::img('@web/img/no-image.png'), ['view', 'name' => $model->name]);
+            echo Html::a(Html::img('@web/img/no-image.png'), ['/mod/view', 'name' => $model->name]);
         }
         ?>
         <div class="caption">
-            <h3><?= Html::a(Html::encode($model->name), ['view', 'name' => $model->name]) ?></h3>
+            <h3><?= Html::a(Html::encode($model->name), ['/mod/view', 'name' => $model->name]) ?></h3>
 
             <p><?= $model->description; ?></p>
 
